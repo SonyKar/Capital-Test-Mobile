fce34d244e56a197312ac1a8c8aaa53b3216e70f
Alex <sa.caraganciu@gmail.com>
2019-03-20 15:14:12 +0200

    - Setting up new 3 questions of 5  - Final commit

--- app/src/main/java/com/BST/capitaltest/MainActivity.java
+++ app/src/main/java/com/BST/capitaltest/MainActivity.java
@@ -10,12 +10,13 @@ public class MainActivity extends AppCompatActivity {
 
     static int points = 0, questionNumber = 0;
 
+    // question, option1, option2, option3, option4, number of correct answer (1-4)
     static String[][] questions = {
         {"The capital of Moldova?", "Balti", "Chisinau", "Cagul", "Calarasi", "2"},
         {"The capital of Russia?", "Moscow", "Peterburg", "Voronej", "Novosibirsk", "1"},
-        {"The capital of Moldova?", "Balti", "Chisinau", "Cagul", "Calarasi", "2"},
-        {"The capital of Moldova?", "Balti", "Chisinau", "Cagul", "Calarasi", "2"},
-        {"The capital of Moldova?", "Balti", "Chisinau", "Cagul", "Calarasi", "2"}
+        {"The capital of Italy?", "Milan", "Rome", "Florence", "Venice", "2"},
+        {"The capital of Turkey?", "Side", "Alania", "Stambul", "Kemer", "3"},
+        {"The capital of Egypt?", "Hurgada", "Asuan", "Alexandria", "Kair", "4"}
     };
 
     @Override
